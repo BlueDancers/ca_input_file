e0a1787ac15c75205137b1ae5a6731ef0da425db
Kai Wu <wu.vkcyan@gmail.com>
2019-12-01 12:05:07 +0800

    提交最初版本

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { Uploader, Button } from "vant";
 
+Vue.use(Uploader);
+Vue.use(Button);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,46 @@
 <template>
   <div class="home">
-    <img class="home_img" alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="active">
+      <van-uploader :before-read="beforeRead" capture="camera">
+        <van-button icon="photo" type="primary">拍照上传</van-button>
+      </van-uploader>
+      <van-uploader :before-read="beforeRead" :multiple="true">
+        <van-button icon="photo" type="primary">多选图片</van-button>
+      </van-uploader>
+    </div>
+    <div>
+      <div v-for="item in file" :key="item.name">
+        {{ item.name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      file: ""
+    };
+  },
+  methods: {
+    beforeRead(file) {
+      console.log(file);
+      this.file = file;
+    }
   }
 };
 </script>
 
-
-
 <style lang="less" scoped>
-.home_img {
-  width: 200px;
-  height: 200px;
+.home {
+  margin-top: 100px;
+  .active {
+    display: flex;
+    justify-content: space-around;
+  }
 }
 </style>
